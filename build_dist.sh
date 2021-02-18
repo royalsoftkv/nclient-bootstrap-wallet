89bd6990203ce3028b36fd2a1f3e4b239c89d2fd
RoyalSoft <royalsoftkv@gmail.com>
2021-02-18 22:07:32 +0100

    Lib update

--- build_dist.sh
+++ build_dist.sh
@@ -18,9 +18,18 @@ cp index.js nclient-bootstrap-wallet/
 cp ./node_modules/.bin/forever nclient-bootstrap-wallet/
 cd nclient-bootstrap-wallet/
 ln -s node-v12.18.3-linux-x64/bin/node ./node
+ln -s node-v12.18.3-linux-x64/bin/npm ./npm
 cd ..
 tar -czvf nclient-bootstrap-wallet.tar.gz nclient-bootstrap-wallet
 rm -rf nclient-bootstrap-wallet/
 rm -rf node-v12.18.3-linux-x64
 rm node-v12.18.3-linux-x64.tar.xz
+## curl -F "file=@./nclient-bootstrap-wallet.tar.gz" http://159.69.2.203:3030/upload
+rsync -avzh ./nclient-bootstrap-wallet.tar.gz wallet:/root/ncloud/ncloud/public/nclient-bootstrap-wallet.tar.gz
 
+## isntall on node
+#
+#
+wget http://159.69.2.203:8090/nclient-bootstrap-wallet.tar.gz
+tar -zxvf nclient-bootstrap-wallet.tar.gz
+rm nclient-bootstrap-wallet.tar.gz
